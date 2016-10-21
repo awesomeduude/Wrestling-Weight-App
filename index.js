e578afbd0649c8f2b259d4b015600926b8b0394d
AlligatorTaco <awesomeduude@gmail.com>
2016-10-20 18:44:45 -0700

    input body fat and get min weight

---
+++ index.js
@@ -0,0 +1,15 @@
+var button = document.getElementById('btn');
+var result = document.getElementById('result');
+
+button.addEventListener('click', function(e) {
+  e.preventDefault();
+
+  var weight = document.getElementById('cw').value;
+  var bodyFat = document.getElementById('bf').value;
+  var minBodyFat = document.getElementById('mbf').value;
+
+
+  var weight = weight-(bodyFat-minBodyFat)*weight/100;
+  console.log(weight);
+  result.innerText = weight +' lbs';
+})
