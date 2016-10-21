96107cd1782d997891bd89cf420ae5c8609fdda5
AlligatorTaco <awesomeduude@gmail.com>
2016-10-20 19:28:49 -0700

    setup express

---
+++ public/bundle.js
@@ -0,0 +1,65 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports) {
+
+	var button = document.getElementById('btn');
+	var result = document.getElementById('result');
+
+	button.addEventListener('click', function(e) {
+	  e.preventDefault();
+
+	  var weight = document.getElementById('cw').value;
+	  var bodyFat = document.getElementById('bf').value;
+	  var minBodyFat = document.getElementById('mbf').value;
+
+
+	  var weight = weight-(bodyFat-minBodyFat)*weight/100;
+	  console.log(weight);
+	  result.innerText = weight +' lbs';
+	})
+
+
+/***/ }
+/******/ ]);
