ce2066086e708698a2d378282d608d738b32c1af
AlligatorTaco <awesomeduude@gmail.com>
2016-10-20 20:26:17 -0700

    listens to correct port

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ app.get('/', function(req,res) {
 });
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log('**********Listening on Port 3000**********');
 });
